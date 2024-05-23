de7dcf6bc7038c5515a6a1d8a90d53a57ae698be
MichaelMDCJR <mcarlson7404@gmail.com>
2024-05-23 12:59:48 -0500

    Printing images to window with tkinter

--- Reddit Scraper.py
+++ Reddit Scraper.py
@@ -19,23 +19,44 @@ root.title("Reddit Scraper")
 
 # Set size of window
 root.geometry('1000x600')
+
+#Create the main frame
+main_frame = Frame(root)
+main_frame.pack(fill=BOTH,expand=1)
+
+#Create canvas
+my_canvas = Canvas(main_frame)
+my_canvas.pack(side=LEFT,fill=BOTH,expand=1)
+
+#Create scrollbar
+y_scrollbar = Scrollbar(main_frame,orient=VERTICAL,command=my_canvas.yview)
+y_scrollbar.pack(side=RIGHT, fill=Y)
+
+my_canvas.configure(yscrollcommand=y_scrollbar.set)
+my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+
+second_frame = Frame(my_canvas,width=10, height=10, bg="lightblue")
+my_canvas.create_window((0,0), window=second_frame,anchor="nw")
+
 posts = []
-numPosts = 10
+numPosts = 30
 
 # Set up the banner
-banner = Label(root, text="Subbreddit goes here",bg="#FF4500", height=1, width=500, anchor=W,
+banner = Label(second_frame, text="Subbreddit goes here",bg="#FF4500", height=1, width=500, anchor=W,
 font=("Times New Roman", 32, "bold"))
 banner.pack()
 
 #To test you must visit https://www.reddit.com/prefs/apps and create an app and input given data
 async def main():
+    postsGrabbed = 0
+
     reddit = asyncpraw.Reddit(client_id="----------------", client_secret="---------------",
                               user_agent="------------")
 
     #print(reddit.read_only)
 
     #posts = []
-    subreddit = await reddit.subreddit("memes")
+    subreddit = await reddit.subreddit("cats")
     async for submission in subreddit.hot(limit=numPosts):
         if ".jpeg" in str(submission.url) or ".jpg" in str(submission.url) or ".png" in str(submission.url):
             print(str(submission.url))
@@ -44,25 +65,23 @@ async def main():
                 "author": str(submission.author),
                 "image": submission.url
             })
-
-    # for i in range(numPosts):
-    #     print(posts[i])
-
-    # toBeDel = []
-    # for i in range(numPosts):
-    #     if ".jpeg" or ".png" or ".jpg" not in posts[i]["image"]:
-    #         del posts[i]
+            postsGrabbed += 1
 
 
 
-    with urllib.request.urlopen("https://i.redd.it/y2r4uhez26sc1.gif") as u:
-        raw_data = u.read()
+    for i in range(postsGrabbed):
+        with urllib.request.urlopen(posts[i]["image"]) as u:
+            raw_data = u.read()
 
-    image = Image.open(io.BytesIO(raw_data))
-    photo = ImageTk.PhotoImage(image)
-    label = Label(root, image=photo)
-    label.image = photo
-    label.pack()
+        image = Image.open(io.BytesIO(raw_data))
+        #resizedImage = image.resize((853, 480))
+        image.thumbnail((500,500))
+        photo = ImageTk.PhotoImage(image)
+        label = Label(second_frame, image=photo, height=510, width=900)
+        label.image = photo
+        label.pack(anchor="w")
+
+
 
     await reddit.close()
 
